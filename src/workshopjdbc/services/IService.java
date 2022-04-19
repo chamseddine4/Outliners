c2f23da19074eacefe59b999d0fe7c4a242d846d
chams <chamseddine.hammami@esprit.tn>
2022-04-19 01:09:56 +0100

    dd
    dz

---
+++ src/workshopjdbc/services/IService.java
@@ -0,0 +1,11 @@
+package workshopjdbc.services;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IService <T> {
+    void ajouter(T t) throws SQLException;
+    List<T> afficher();
+    void modifier(T t);
+    void supprimer(T t);
+}
